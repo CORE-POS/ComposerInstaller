03317f21d9f0845878665b254ec3cb3199a55acd
Andy Theuninck <andy@gohanman.com>
2021-09-17 11:34:18 -0500

    Updating for Composer class api differences

--- test/Test.php
+++ test/Test.php
@@ -5,6 +5,8 @@ use Composer\Config;
 use Composer\Installer\InstallationManager;
 use Composer\IO\NullIO;
 use Composer\Package\Package;
+use Composer\Util\Loop;
+use Composer\Util\HttpDownloader;
 use COREPOS\ComposerInstaller\Installer;
 use COREPOS\ComposerInstaller\Plugin;
 use PHPUnit\Framework\TestCase;
@@ -15,8 +17,9 @@ class Test extends TestCase
     {
         $c = new Composer();
         $c->setConfig(new Config());
-        $c->setInstallationManager(new InstallationManager());
         $io = new NullIO();
+        $loop = new Loop(new HttpDownloader($io, new Config()));
+        $c->setInstallationManager(new InstallationManager($loop, $io));
 
         $plugin = new Plugin();
         $plugin->activate($c, $io);
@@ -26,8 +29,9 @@ class Test extends TestCase
     {
         $c = new Composer();
         $c->setConfig(new Config());
-        $c->setInstallationManager(new InstallationManager());
         $io = new NullIO();
+        $loop = new Loop(new HttpDownloader($io, new Config()));
+        $c->setInstallationManager(new InstallationManager($loop, $io));
 
         $installer = new Installer($io, $c);
         $this->assertEquals(true, $installer->supports('corepos-lane-plugin'));
